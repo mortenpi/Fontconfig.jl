1538212740eebcfdef0ca9d8ab603bad0fe59298
Morten Piibeleht <morten.piibeleht@gmail.com>
2015-11-14 10:44:58 +0100

    Add deps

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,10 @@
 using BinDeps
 @BinDeps.setup
 
+deps = [
+    fontconfig = library_dependency("fontconfig", aliases = ["libfontconfig-1", "libfontconfig", "libfontconfig.so.1"], depends = [freetype], runtime = false, group = group)
+]
+
 @osx_only begin
     if Pkg.installed("Homebrew") === nothing
         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
