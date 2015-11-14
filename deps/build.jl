b843dbfc86ae78ed665ca9b3dfe54133db860234
Morten Piibeleht <morten.piibeleht@gmail.com>
2015-11-14 11:01:39 +0100

    ...

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,7 @@ using BinDeps
 @BinDeps.setup
 
 deps = [
+    freetype = library_dependency("freetype", aliases = ["libfreetype"], runtime = false, group = group)
     fontconfig = library_dependency("fontconfig", aliases = ["libfontconfig-1", "libfontconfig", "libfontconfig.so.1"], depends = [freetype], runtime = false, group = group)
 ]
 
@@ -10,5 +11,8 @@ deps = [
         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
     end
     using Homebrew
+    provides( Homebrew.HB, "freetype", freetype, os = :Darwin )
     provides( Homebrew.HB, "fontconfig", fontconfig, os = :Darwin )
 end
+
+@BinDeps.install Dict([(:fontconfig, :_jl_libfontconfig)])
