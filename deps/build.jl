4ec87f9cfe98f14a5206a90b709a65008f7dc37e
Morten Piibeleht <morten.piibeleht@gmail.com>
2015-11-14 09:42:32 +0100

    Install fontconfig with Homebrew automatically

---
+++ deps/build.jl
@@ -0,0 +1,7 @@
+@osx_only begin
+    if Pkg.installed("Homebrew") === nothing
+        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
+    end
+    using Homebrew
+    provides( Homebrew.HB, "fontconfig", fontconfig, os = :Darwin )
+end
